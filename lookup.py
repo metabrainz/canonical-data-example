016392697d4cafe5fabcb1680edcf71b4870ffba
Robert Kaye <rob@musicbrainz.org>
2023-05-09 21:06:26 +0200

    Finish debugging -- working as expected now.

--- lookup.py
+++ lookup.py
@@ -18,6 +18,7 @@ def make_combined_lookup(artist_name, recording_name):
     """ Given the artist name and recording name, return a combined_lookup string """
     return unidecode(re.sub(r'[^\w]+', '', artist_name + recording_name).lower())
 
+
 def get_client():
     return typesense.Client({
         'nodes': [{
@@ -30,11 +31,9 @@ def get_client():
     })
 
 
-
 def build_index(csv_file):
     """ Import the MusicBrainz Canonical Data CSV file into typesense """
 
-    max_score = 4 * 1024 * 1024 * 1024  # a sufficiently large number
     client = get_client()
     schema = {
         'name': TYPESENSE_COLLECTION,
@@ -58,16 +57,17 @@ def build_index(csv_file):
             for i, row in enumerate(csv.reader(csv_file, delimiter=',')):
                 if i == 0:
                     continue
+
                 document = {
                     'artist_credit_id': row[1],
-                    'artist_mbids': "{" + row[2] + "}",
+                    'artist_mbids': row[2],
                     'artist_credit_name': row[3],
                     'release_mbid': row[4],
                     'release_name': row[5],
                     'recording_mbid': row[6],
                     'recording_name': row[7],
-                    'score': max_score - int(row[9]),
-                    'combined': make_combined_lookup(row[7], row[3])
+                    'combined': row[8],
+                    'score': int(row[9])
                 }
                 documents.append(document)
 
@@ -82,43 +82,41 @@ def build_index(csv_file):
     except typesense.exceptions.TypesenseClientError as err:
         print("typesense index: Cannot build index: ", str(err))
 
+
 def lookup(query):
 
     client = get_client()
-    search_parameters = {
-        'q': query,
-        'query_by': "combined",
-        'prefix': 'no',
-        'num_typos': 5
-    }
+    search_parameters = {'q': query, 'query_by': "combined", 'prefix': 'no', 'num_typos': 5}
 
     hits = client.collections[TYPESENSE_COLLECTION].documents.search(search_parameters)
 
     output = []
     for hit in hits['hits']:
-        output.append({'artist_credit_name': hit['document']['artist_credit_name'],
-                       'artist_mbids': hit['document']['artist_mbids'],
-                       'release_name': hit['document']['release_name'],
-                       'release_mbid': hit['document']['release_mbid'],
-                       'recording_name': hit['document']['recording_name'],
-                       'recording_mbid': hit['document']['recording_mbid']})
+        output.append({
+            'artist_credit_name': hit['document']['artist_credit_name'],
+            'artist_mbids': hit['document']['artist_mbids'],
+            'release_name': hit['document']['release_name'],
+            'release_mbid': hit['document']['release_mbid'],
+            'recording_name': hit['document']['recording_name'],
+            'recording_mbid': hit['document']['recording_mbid']
+        })
 
     return output
+
 
 def lookup_track(artist_name, recording_name):
     """ Lookup a track given the artist_name and recording_name. Print results to stdout. """
 
     matches = lookup(make_combined_lookup(artist_name, recording_name))
     if len(matches) == 0:
-        print("track was not found")
+        print("no canonical recording found")
     else:
         match = matches[0]
-        print("track was matched:")
-        print(f"  artist: {match['artist_credit_name']}")
-        print(f"  release_mbid: {match['artist_mbids']}")
-        print(f"  release: {match['release_name']}")
-        print(f"  release_mbid: {match['release_mbid']}")
-        print(f"  recording: {match['recording_name']}")
+        print(f"          artist: {match['artist_credit_name']}")
+        print(f"         release: {match['release_name']}")
+        print(f"       recording: {match['recording_name']}")
+        print(f"    artist_mbids: {match['artist_mbids']}")
+        print(f"    release_mbid: {match['release_mbid']}")
         print(f"  recording_mbid: {match['recording_mbid']}")
 
 
